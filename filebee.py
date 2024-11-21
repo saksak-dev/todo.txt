77fd1f65c1403eb6e8c97f787746632a701a8ddc
saksak-dev <saksak13131313@gmail.com>
2024-11-21 13:50:29 +0100

    file bee starting out

--- filebee.py
+++ filebee.py
@@ -1,58 +1,52 @@
-from fileinput import filename
-
-
 class FileBeeeeee:
     def __init__(self, name):
         self.filename = name
 
     def line_list(self):
         with open(self.filename, 'r') as reader:
-            lines = reader.readlines()
-        return lines
+            return reader.readlines()
 
     def write_to_doc(self, lines):
         with open(self.filename, 'w') as writer:
-            for line in lines:
-                writer.write(line + '\n')
+            writer.writelines(lines)
 
-
-    def date_dis(self,date):
+    def format_date(self, date):
         return 'dddd' + date
 
     def print_day(self, date):
         lines = self.line_list()
-        date_formated = self.date_dis(date)
+        date_formatted = self.format_date(date)
         day_start = -1
         for index, line in enumerate(lines):
-            if date_formated in line:
+            if date_formatted in line.strip():
                 day_start = index
+                break
         if day_start == -1:
             print('Date not found')
         else:
             print(date)
             counter = 1
-            for line in lines[day_start+1:]:
+            for line in lines[day_start + 1:]:
                 if 'dddd' in line:
                     break
-                else:
-                    print(f'{counter}. {line.replace('[===done===]', '[done]')}')
-                    counter+=1
-
+                print(f'{counter}. {line.strip().replace("[===done===]", "[done]")}')
+                counter += 1
 
     def return_date_index(self, date):
         lines = self.line_list()
-        date_formated = self.date_dis(date)
-        day_start = -1
+        date_formatted = self.format_date(date)
         for index, line in enumerate(lines):
-            if date_formated in line:
-                day_start = index
-        return day_start
-
+            if date_formatted in line.strip():
+                return index
+        return -1
 
     def finish_task(self, index, date):
-        lines=self.line_list()
-        if self.return_date_index(date) == -1:
+        lines = self.line_list()
+        date_index = self.return_date_index(date)
+        if date_index == -1:
             print('Date does not exist')
             return
-        lines[self.return_date_index(date)+index]+='[===done===]'
+        task_index = date_index + index
+        if '[===done===]' not in lines[task_index]:
+            lines[task_index] = lines[task_index].strip() + '[===done===]\n'
         self.write_to_doc(lines)

55d6ab4b495ae5fd2a67ebe4513a7a64fbf6867f
saksak-dev <saksak13131313@gmail.com>
2024-11-21 13:47:05 +0100

    file bee starting out

--- filebee.py
+++ filebee.py
@@ -10,10 +10,14 @@ class FileBeeeeee:
             lines = reader.readlines()
         return lines
 
+    def write_to_doc(self, lines):
+        with open(self.filename, 'w') as writer:
+            for line in lines:
+                writer.write(line + '\n')
+
+
     def date_dis(self,date):
-        newdate = date.replace('.','')
-        newdate = newdate.replace('.','')
-        return 'dddd' + newdate
+        return 'dddd' + date
 
     def print_day(self, date):
         lines = self.line_list()
@@ -35,10 +39,20 @@ class FileBeeeeee:
                     counter+=1
 
 
-
-
-
+    def return_date_index(self, date):
+        lines = self.line_list()
+        date_formated = self.date_dis(date)
+        day_start = -1
+        for index, line in enumerate(lines):
+            if date_formated in line:
+                day_start = index
+        return day_start
 
 
     def finish_task(self, index, date):
-        lines=line.list()
+        lines=self.line_list()
+        if self.return_date_index(date) == -1:
+            print('Date does not exist')
+            return
+        lines[self.return_date_index(date)+index]+='[===done===]'
+        self.write_to_doc(lines)

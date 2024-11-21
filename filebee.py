4421fd67e44e8e86b00b89b9884d5bcfa3115e32
saksak-dev <saksak13131313@gmail.com>
2024-11-21 09:56:57 +0100

    file bee starting out

--- filebee.py
+++ filebee.py
@@ -13,11 +13,11 @@ class FileBeeeeee:
     def date_dis(self,date):
         newdate = date.replace('.','')
         newdate = newdate.replace('.','')
-        return newdate
+        return 'dddd' + newdate
 
     def print_day(self, date):
         lines = self.line_list()
-        date_formated = 'dddd' + self.date_dis(date)
+        date_formated = self.date_dis(date)
         day_start = -1
         for index, line in enumerate(lines):
             if date_formated in line:
@@ -30,8 +30,10 @@ class FileBeeeeee:
             for line in lines[day_start+1:]:
                 if 'dddd' in line:
                     break
-                print(f'{counter}. {line}')
-                counter+=1
+                else:
+                    print(f'{counter}. {line.replace('[===done===]', '[done]')}')
+                    counter+=1
+
 
 
 

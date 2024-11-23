6d9f19c97cf4692ca47fe00dab9e88fd62d1dc80
saksak-dev <saksak13131313@gmail.com>
2024-11-23 07:20:25 +0100

    file bee starting out

--- filebee.py
+++ filebee.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 class FileBeeeeee:
     def __init__(self, name):
         self.filename = name
@@ -10,43 +11,5 @@ class FileBeeeeee:
         with open(self.filename, 'w') as writer:
             writer.writelines(lines)
 
-    def format_date(self, date):
-        return 'dddd' + date
 
-    def print_day(self, date):
-        lines = self.line_list()
-        date_formatted = self.format_date(date)
-        day_start = -1
-        for index, line in enumerate(lines):
-            if date_formatted in line.strip():
-                day_start = index
-                break
-        if day_start == -1:
-            print('Date not found')
-        else:
-            print(date)
-            counter = 1
-            for line in lines[day_start + 1:]:
-                if 'dddd' in line:
-                    break
-                print(f'{counter}. {line.strip().replace("[===done===]", "[done]")}')
-                counter += 1
 
-    def return_date_index(self, date):
-        lines = self.line_list()
-        date_formatted = self.format_date(date)
-        for index, line in enumerate(lines):
-            if date_formatted in line.strip():
-                return index
-        return -1
-
-    def finish_task(self, index, date):
-        lines = self.line_list()
-        date_index = self.return_date_index(date)
-        if date_index == -1:
-            print('Date does not exist')
-            return
-        task_index = date_index + index
-        if '[===done===]' not in lines[task_index]:
-            lines[task_index] = lines[task_index].strip() + '[===done===]\n'
-        self.write_to_doc(lines)
